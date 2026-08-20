72c9801521a691302b06d07245dd38fd3be08d5b
ashwini272-d <ashwini31027@gmail.com>
2026-08-20 23:50:52 +0530

    Time: 0 ms (100%), Space: 42.5 MB (51.51%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1 && n!=4){
+            int sum=0;
+            while(n>0){
+                int rem=n%10;
+                sum=sum+(rem*rem);
+                n=n/10;
+            }
+            n=sum;
+        }
+        return n==1;
+    }
+    //public static void main(String[] args){
+
+}
